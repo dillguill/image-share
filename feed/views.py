b1631db93bee2f91a83d0b7ab5120d8f10b10186
Dillon <162556809+dillguill@users.noreply.github.com>
2025-01-03 19:09:24 -0600

    8. Custom Page Context

--- feed/views.py
+++ feed/views.py
@@ -2,3 +2,9 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['my_thing'] = 'Hello World'
+        return context
+    
